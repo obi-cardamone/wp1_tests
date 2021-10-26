8415c9d8146b20d8cbe09da0560bd28e879c7820
obi-cardamone <salvatore@comind.io>
2021-10-26 12:52:43 +0100

    Added optimising flag to compilation

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-clang++ -std=c++17 -fpic -o my_low_level_library.so -shared ctypes/my_low_level_library.cpp -lpthread
+clang++ -std=c++17 -fpic -O3 -o my_low_level_library.so -shared ctypes/my_low_level_library.cpp -lpthread
 echo "Running ctypes test with low-level C++ implementation..."
 time python3 ctypes/producer.py "cpp"
 echo "Running ctypes test with numpy implementation..."
